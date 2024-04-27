30cbbd3562c599b3905c1cdf7f18c07f9beba3ea
dam-ari <82123617+dam-ari@users.noreply.github.com>
2024-04-27 19:11:07 +0000

    Update theme service to toggle between light and dark themes

--- starry-sky-app/src/app/app.component.ts
+++ starry-sky-app/src/app/app.component.ts
@@ -2,18 +2,25 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 // import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SettingsComponent } from './settings/settings.component';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     // BrowserAnimationsModule, 
-    RouterModule, 
-    SettingsComponent ],
+    RouterModule,
+    SettingsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [ThemeService]
+
 })
 export class AppComponent {
   title = 'starry-sky-app';
-  
+  constructor(public themeService: ThemeService) { }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
 }
--- starry-sky-app/src/app/theme.service.ts
+++ starry-sky-app/src/app/theme.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@angular/core';
+// Import PLATFORM_ID and isPlatformBrowser
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
+  // system default
+  private theme: 'light' | 'dark' = 'light';
+ // get the system default theme
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadTheme();  // Only load theme if on the client-side
+    }
+  }
+
+  toggleTheme(): void {
+    if (!isPlatformBrowser(this.platformId)) return; // Guard against server-side invocation
+
+    this.theme = (this.theme === 'light' ? 'dark' : 'light');
+    localStorage.setItem('theme', this.theme);
+    this.updateBodyClass();
+    alert(this.theme)
+  }
+
+  private loadTheme(): void {
+    const storedTheme = localStorage.getItem('theme');
+    this.theme = (window.matchMedia('(prefers-color-scheme: dark)').matches 
+    ? 'dark' : 'light')
+    // this.theme = (storedTheme === 'light' || storedTheme === 'dark') ? storedTheme : this.theme;
+    //update only if the stored theme is different from the system default
+    // if (this.theme !== storedTheme) {
+      this.updateBodyClass();
+    // }
+  }
+
+  private updateBodyClass(): void {
+    var body = document.body;
+      const themeToRemove = this.theme === 'light' ? 'dark' : 'light';
+      body.classList.remove(themeToRemove);
+      body.classList.add(this.theme);
+
+  }
 }
